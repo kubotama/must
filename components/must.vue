5b2cdb59f90a66a5c5753a4396d4c194c5ea99f1
kubotama <16697573+kubotama@users.noreply.github.com>
2019-12-20 16:37:42 +0900

    特殊文字の変換を追加

--- components/must.vue
+++ components/must.vue
@@ -28,8 +28,24 @@ export default {
     }
   },
   methods: {
+    escapeSpecialChar(text) {
+      text = text.replace(/\\/g, '\\textbackslash ')
+      // eslint-disable-next-line no-useless-escape
+      text = text.replace(/#/g, '\\#')
+      text = text.replace(/\$/g, '\\$')
+      text = text.replace(/%/g, '\\%')
+      text = text.replace(/&/g, '\\&')
+      text = text.replace(/~/g, '\\textasciitilde ')
+      text = text.replace(/_/g, '\\_')
+      text = text.replace(/\^/g, '\\textasciicircum ')
+      text = text.replace(/{/g, '\\{')
+      text = text.replace(/}/g, '\\}')
+
+      text = text.trim()
+      return text
+    },
     clickEscapeSpecialChar() {
-      this.mustArea = '\\#'
+      this.mustArea = this.escapeSpecialChar(this.mustArea)
     }
   }
 }
