d392f9ca35a47c4256a59e7e668711cfe71497b4
kubotama <16697573+kubotama@users.noreply.github.com>
2019-12-26 00:19:37 +0900

    脚注の作成
    改行と特殊文字が連続するテストケース

--- test/must_23.spec.js
+++ test/must_23.spec.js
@@ -10,13 +10,14 @@ describe('issue #23', () => {
   })
 
   test.each`
-    id   | beforeText          | afterText
-    ${1} | ${'特殊文字なし'}   | ${'\\footnote{特殊文字なし}'}
-    ${2} | ${'特殊文字あり#'}  | ${'\\footnote{特殊文字あり\\#}'}
-    ${3} | ${'特殊$文字あり'}  | ${'\\footnote{特殊\\$文字あり}'}
-    ${4} | ${'特殊\\文字あり'} | ${'\\footnote{特殊\\textbackslash 文字あり}'}
-    ${5} | ${'あい\nう\nえお'} | ${'\\footnote{あい う えお}'}
-    ${6} | ${'あいう\n\nえ\nお'} | ${'\\footnote{あいう え お}'}
+    id   | beforeText              | afterText
+    ${1} | ${'特殊文字なし'}       | ${'\\footnote{特殊文字なし}'}
+    ${2} | ${'特殊文字あり#'}      | ${'\\footnote{特殊文字あり\\#}'}
+    ${3} | ${'特殊$文字あり'}      | ${'\\footnote{特殊\\$文字あり}'}
+    ${4} | ${'特殊\\文字あり'}     | ${'\\footnote{特殊\\textbackslash 文字あり}'}
+    ${5} | ${'あい\nう\nえお'}     | ${'\\footnote{あい う えお}'}
+    ${6} | ${'あいう\n\nえ\nお'}   | ${'\\footnote{あいう え お}'}
+    ${7} | ${'あい\n#う$\nえお\n'} | ${'\\footnote{あい \\#う\\$ えお}'}
   `('$id: $beforeText → $afterText', ({ id, beforeText, afterText }) => {
     wrapper.setData({ mustArea: beforeText })
     wrapper.find(buttonId).trigger('click')
