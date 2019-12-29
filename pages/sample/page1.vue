762f54df25bc94f064d8f2383fbc6380f7fc7e98
kubotama <16697573+kubotama@users.noreply.github.com>
2019-12-29 15:44:21 +0900

    テスト用のページを作成
    自動テストではなく目視での確認のみ。

---
+++ pages/sample/page1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Page title="タイトル1" />
+  </div>
+</template>
+
+<script>
+import Page from '~/components/Page.vue'
+
+export default {
+  components: {
+    Page
+  }
+}
+</script>
