f2b3c678ceab861c46b79951713556e5eacc587d
kubotama <16697573+kubotama@users.noreply.github.com>
2020-01-05 16:29:44 +0900

    指定されたurlのタイトルを取得

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import url from 'url'
+import { JSDOM } from 'jsdom'
+// import axios from '@nuxtjs/axios'
+import request from 'request'
 
 /* eslint-disable no-console */
 export default (req, res, next) => {
@@ -7,6 +10,25 @@ export default (req, res, next) => {
   const targetUrl = url.parse(req.url, true).query.url
   console.log('target url: ' + targetUrl)
 
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({ title: 'タイトル1' }))
+  // JSDOM.fromURL(targetUrl).then((dom) => {
+  //   const title = dom.window.document.getElementsByTagName('title')[0]
+  //     .textContent
+  //   res.writeHead(200, { 'Content-Type': 'application/json' })
+  //   res.end(JSON.stringify({ title }))
+  // })
+
+  // axios.get(targetUrl).then((response) => {
+  // console.log(response.data)
+  // const dom = new JSDOM(response.data)
+  request(targetUrl, (e, response, body) => {
+    if (e) {
+      console.error(e)
+    }
+    const dom = new JSDOM(body)
+    const title = dom.window.document.getElementsByTagName('title')[0]
+      .textContent
+    console.log(`title: ${title}`)
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ title }))
+  })
 }
