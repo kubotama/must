305e781759d2a232268e890cd24ec47b264cc457
kubotama <16697573+kubotama@users.noreply.github.com>
2020-01-05 15:39:36 +0900

    urlからターゲットのurlを取り出しを追加
    new URL()ではエラーになる理由が不明のため、暫定的にlegacy APIのurl.parseを利用している。
    serverMiddlewareで指定されたスクリプト(=サーバで実行)のため、動作確認用のconsole.logを残す。

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
+import url from 'url'
+
 /* eslint-disable no-console */
 export default (req, res, next) => {
-  // console.log('header: ' + JSON.stringify(req.headers))
   console.log('url: ' + JSON.stringify(req.url))
-  // res.status(200).end(JSON.stringify({ title: 'Google' }))
+  // eslint-disable-next-line node/no-deprecated-api
+  const targetUrl = url.parse(req.url, true).query.url
+  console.log('target url: ' + targetUrl)
+
   res.writeHead(200, { 'Content-Type': 'application/json' })
-  // res.write()
   res.end(JSON.stringify({ title: 'タイトル1' }))
 }
