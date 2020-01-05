38522027a47f65a7ef5d4c33f070ec0741371f62
kubotama <16697573+kubotama@users.noreply.github.com>
2020-01-05 16:30:29 +0900

    無駄なコメントを削除

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import url from 'url'
 import { JSDOM } from 'jsdom'
-// import axios from '@nuxtjs/axios'
 import request from 'request'
 
 /* eslint-disable no-console */
@@ -10,16 +9,6 @@ export default (req, res, next) => {
   const targetUrl = url.parse(req.url, true).query.url
   console.log('target url: ' + targetUrl)
 
-  // JSDOM.fromURL(targetUrl).then((dom) => {
-  //   const title = dom.window.document.getElementsByTagName('title')[0]
-  //     .textContent
-  //   res.writeHead(200, { 'Content-Type': 'application/json' })
-  //   res.end(JSON.stringify({ title }))
-  // })
-
-  // axios.get(targetUrl).then((response) => {
-  // console.log(response.data)
-  // const dom = new JSDOM(response.data)
   request(targetUrl, (e, response, body) => {
     if (e) {
       console.error(e)
