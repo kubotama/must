77bd091bcca60428aac150c6af1d74119fa114ee
kubotama <16697573+kubotama@users.noreply.github.com>
2020-01-08 00:17:27 +0900

    リファクタリング
    コメントアウトを削除

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import url from 'url'
 import { JSDOM } from 'jsdom'
-// import request from 'request'
 import HttpStatus from 'http-status-codes'
 import axios from 'axios'
 
@@ -11,17 +10,6 @@ export default (req, res, next) => {
   const targetUrl = url.parse(req.url, true).query.url
   console.log('target url: ' + targetUrl)
 
-  // request(targetUrl, (e, response, body) => {
-  //   if (e) {
-  //     console.error(e)
-  //   }
-  //   const dom = new JSDOM(body)
-  //   const title = dom.window.document.getElementsByTagName('title')[0]
-  //     .textContent
-  //   console.log(`title: ${title}`)
-  //   res.writeHead(HttpStatus.OK, { 'Content-Type': 'application/json' })
-  //   res.end(JSON.stringify({ title }))
-  // })
   axios.get(targetUrl).then((response) => {
     const body = response.data
 
